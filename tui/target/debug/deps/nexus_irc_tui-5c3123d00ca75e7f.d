6172f58b77aa72746abba0681744dd1a0394972e
Trent Slade <73952179+EmergentMonk@users.noreply.github.com>
2026-08-09 15:43:10 +0930

    Remove tracked Rust build artifacts

--- tui/target/debug/deps/nexus_irc_tui-5c3123d00ca75e7f.d
+++
@@ -1,6 +0,0 @@
-/home/runner/work/QSOL-NEXUS/QSOL-NEXUS/tui/target/debug/deps/nexus_irc_tui-5c3123d00ca75e7f.d: src/lib.rs src/scripting.rs
-
-/home/runner/work/QSOL-NEXUS/QSOL-NEXUS/tui/target/debug/deps/libnexus_irc_tui-5c3123d00ca75e7f.rmeta: src/lib.rs src/scripting.rs
-
-src/lib.rs:
-src/scripting.rs:
--- tui/target/debug/deps/type1_encoding_parser-9c966fdf66816db3.d
+++
@@ -1,5 +0,0 @@
-/home/runner/work/QSOL-NEXUS/QSOL-NEXUS/tui/target/debug/deps/type1_encoding_parser-9c966fdf66816db3.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/type1-encoding-parser-0.1.1/src/lib.rs
-
-/home/runner/work/QSOL-NEXUS/QSOL-NEXUS/tui/target/debug/deps/libtype1_encoding_parser-9c966fdf66816db3.rmeta: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/type1-encoding-parser-0.1.1/src/lib.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/type1-encoding-parser-0.1.1/src/lib.rs:
