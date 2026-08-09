afeed02518bba656fc8a5b323f1d953e1d1d52c0
Trent Slade <73952179+EmergentMonk@users.noreply.github.com>
2026-08-09 19:41:29 +0930

    bump runtime to alpha10.3

--- src/nexus_runtime/version.py
+++ src/nexus_runtime/version.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 PROTOCOL_VERSION = "nexus/0.14"
-RUNTIME_VERSION = "2.0.0-alpha10.2"
+RUNTIME_VERSION = "2.0.0-alpha10.3"
 
 
 __all__ = ["PROTOCOL_VERSION", "RUNTIME_VERSION"]
